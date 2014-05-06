1098dd1da95ece316e99ea2401293bd7fcbd8aca
greenmachine1 <scary_penguins05@hotmail.com>
2014-05-05 21:08:51 -0700

    removed the v2 version of my game app and replaced with v3

---
+++ iOS_Game_Project/iOS_Game_Project/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  iOS_Game_Project
+//
+//  Created by Cory Green on 5/5/14.
+//  Copyright Cory Green 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ iOS_Game_Project/iOS_Game_Project/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS_Game_Project
+//
+//  Created by Cory Green on 5/5/14.
+//  Copyright Cory Green 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
